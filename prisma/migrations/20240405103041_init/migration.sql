0ac4ae2fd5dbf325bd6ecd4929b679b81e7c1b52
Shruti Gajbhiye <shrutigajbhiye2209@gmail.com>
2024-04-05 16:15:22 +0530

    Changes in Migration

--- prisma/migrations/20240405103041_init/migration.sql
+++ prisma/migrations/20240405103041_init/migration.sql
@@ -1,13 +1,17 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT,
+    "name" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "verified" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
